cdf5e9e0efdb719747122542c33b53eaf22f6ac9
Sylvio3365 <sylvioandri@gmail.com>
2025-07-06 12:41:19 +0300

    commit

--- biblio/db/script/table.sql
+++ biblio/db/script/table.sql
@@ -58,4 +58,39 @@ CREATE TABLE profil (
     nom VARCHAR(255)
 );
 
-CREATE TABLE adherent
+CREATE TABLE adherent (
+    idadherent INT AUTO_INCREMENT,
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
+    dtn DATE,
+    idutilisateur INT NOT NULL,
+    idprofil INT,
+    PRIMARY KEY (idadherent),
+    UNIQUE (idutilisateur),
+    FOREIGN KEY (idutilisateur) REFERENCES utilisateur (idutilisateur),
+    FOREIGN KEY (idprofil) REFERENCES profil (idprofil)
+);
+
+CREATE TABLE utilisateur (
+    idutilisateur INT AUTO_INCREMENT,
+    username VARCHAR(250),
+    password VARCHAR(250),
+    idadherent INT,
+    idrole INT NOT NULL,
+    PRIMARY KEY (idutilisateur),
+    FOREIGN KEY (idadherent) REFERENCES adherent (idadherent),
+    FOREIGN KEY (idrole) REFERENCES role (idrole)
+);
+
+CREATE TABLE livre (
+    idlivre INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    auteur VARCHAR(255)
+);
+
+CREATE TABLE exemplaire (
+    idexemplaire INT AUTO_INCREMENT PRIMARY KEY,
+    idlivre INT NOT NULL,
+    numero VARCHAR(50) NOT NULL,
+    FOREIGN KEY (idlivre) REFERENCES livre (idlivre)
+);
